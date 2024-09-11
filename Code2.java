9087ebd32e80634ac2b8471ab7d7d8f1efb1e74d
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-11 12:34:26 +0530

    Create Code2.java

---
+++ Code2.java
@@ -0,0 +1,17 @@
+//2. Write a Java program that creates a `Vector` of doubles.
+    // Add five random double values to the vector. Check if the value 10.5 is in the vector.
+    // Print the size of the vector before and after adding an additional value.
+package vector;
+import java.util.Vector;
+
+public class Code2 {
+    
+    public static void main(String[] args) {
+        Vector<Double> sc=new Vector<>();
+        doubles.add(11.9);
+        doubles.add(34.56);
+        doubles.add(-10.75);
+        doubles.add(35.77);
+        doubles.add(13.763);
+    }
+}
